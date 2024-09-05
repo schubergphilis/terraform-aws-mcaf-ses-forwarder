db5f3b0938c01a5049816d24b353cd343f3a56fc
M Bezemer <marcel@beestig.nl>
2024-09-05 13:45:56 +0200

    Warning: Missing version constraint for provider "archive" in `required_providers` (terraform_required_providers)

--- versions.tf
+++ versions.tf
@@ -7,5 +7,9 @@ terraform {
       source                = "hashicorp/aws"
       version               = ">= 4.9.0"
     }
+    archive = {
+      source  = "hashicorp/archive"
+      version = ">= 2.0.0"
+    }
   }
 }
