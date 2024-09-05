24625b33dc69d0ac4f47460425508fbaef702777
M Bezemer <marcel@beestig.nl>
2024-09-05 16:50:07 +0200

    add example

---
+++ examples/main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+
+module "ses-root-accounts-mail-forward" {
+  #checkov:skip=CKV_AWS_19: False positive: https://github.com/bridgecrewio/checkov/issues/3847. The S3 bucket created by this module is encrypted with KMS.
+  #checkov:skip=CKV_AWS_145: False positive: https://github.com/bridgecrewio/checkov/issues/3847. The S3 bucket created by this module is encrypted with KMS.
+  #checkov:skip=CKV_AWS_272: This module does not support lambda code signing at the moment
+  providers = { aws = aws }
+
+  source            = "github.com/schubergphilis/terraform-aws-mcaf-ses-forwarder?ref=v0.1.0"
+  bucket_name       = "ses-forwarder-bucket"
+  from_email        = "example@mail.something"
+  recipient_mapping = {}
+  tags              = {}
+}
